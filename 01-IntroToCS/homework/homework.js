833c6be185834c0a4a1f3535554b1a4a43e108f6
JuliPerezApiwan <julieta.perezapiwan@gmail.com>
2022-08-18 13:54:41 -0300

    01-homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,14 +1,27 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  function BinarioADecimal(num) {
+    let numerogirado = num.split("").reverse().join("");
+    let result = 0;
+    for (let i = 0; i < numerogirado.length; i++){
+     result = result + parseInt(numerogirado[i]) * Math.pow (2, i);
+       }
+       return result;  
+      }
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  //let binario =  num.toString(2)
+  // return binario
+  let result = "";
+  while (num != 0){
+    result = (num % 2) + result;
+    num = Math.floor(num/2)
+    
+} return result;
 }
+
 
 
 module.exports = {
